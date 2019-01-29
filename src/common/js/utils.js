ac56894b0fc96cf0e1d62dbbfe5432f953eb3f7d
毛佳 <maojia@beeplay123.com>
2019-01-29 17:06:03 +0800

    修复

--- src/common/js/utils.js
+++ src/common/js/utils.js
@@ -92,7 +92,7 @@ export default {
         }
         if (localStorage.getItem('APP_CHANNEL') == '100006') {
             location.href =
-                '../../../channel/newokooo'+item.url.startsWith('/')?item.url:'/'+item.url+'/' +
+                '//wap.beeplay123.com/channel/newokooo'+item.url+'/' +
                 '?channel=' +
                 localStorage.getItem('APP_CHANNEL') +
                 '&token=' +
