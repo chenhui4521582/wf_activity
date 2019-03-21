0f66e5f0fdcca2444a4ac8fee2ffebefc0815f07
chenhui <461822621@qq.com>
2019-03-21 20:49:06 +0800

    update

--- src/views/taskgames/poplog.vue
+++ src/views/taskgames/poplog.vue
@@ -22,7 +22,8 @@
         </div>
         <div class="content-box" v-else >
             <img class="bg" src="./img/dialog/bg.png" alt="">
-            <div class="content" v-if="!(newTaskItems&&newTaskItems.popUp)">
+            <div v-if="!newUserTaskFinish">
+              <div class="content" v-if="!(newTaskItems&&newTaskItems.popUp)">
                 <img class="title" src="./img/dialog/congratulations-text.png" alt="">
                 <div class="icon" >
                     <img class="hb-icon" :src="awardItem.awardsImage" alt=""  >
@@ -35,13 +36,24 @@
                     	<div class="progress-bar" :style="{width:motherTask.hasFinishedNum/motherTask.allTaskNum * 100 + '%'}"></div><span>{{motherTask.hasFinishedNum}}/{{motherTask.allTaskNum}}</span></div>
                 </div>
                 <div class="close" @click="close">朕收下了</div>
+              </div>
+              <div class="content" v-else>
+                  <img class="title" src="./img/dialog/new-user-task-text.png" alt="">
+                  <img class="hb-icon" src="./img/dialog/hb-icon.png" alt="">
+                  <p class="num">30元话费</p>
+                  <div class="text">完成新人任务即可获得话费</div>
+                  <div class="close" @click="close">朕知道了</div>
+              </div>
             </div>
-            <div class="content" v-else>
-                <img class="title" src="./img/dialog/new-user-task-text.png" alt="">
-                <img class="hb-icon" src="./img/dialog/hb-icon.png" alt="">
-                <p class="num">30元话费</p>
-                <div class="text">完成新人任务即可获得话费</div>
-                <div class="close" @click="close">朕知道了</div>
+            <div v-else>
+              <div class="new-user-taks-toask">
+                <div class="content">
+                  <img src="./img/toast_title.png" alt="" class="title">
+                  <img src="./img/signIn-icon.png" alt="" class="price">
+                  <div class="num">5元话费</div>
+                  <div class="btn" @click="close">去赚更多奖励</div>
+                </div>
+              </div>
             </div>
         </div>
 	</div>
@@ -57,35 +69,40 @@
 			},
 			newTaskItems: {
 				default: null
-            },
-            crushTaskList: {
+      },
+      crushTaskList: {
 				default: null
-            },
+      },
 			isNewTask: {
 				default: false
-            },
-            masterTask : {
+      },
+      masterTask : {
 				default: false
-            },
-		},
-		mounted() {
-            
-		},
+      },
+      newUserTaskFinish: Boolean
+    },
 		methods: {
 			close(){
-                if(this.masterTask){
-                    let index = this.awardItem && this.awardItem.index,
-                        curTaskList = this.crushTaskList.allTask[index]
-                    if(this.awardItem && this.awardItem.showMedalImg){
-                        this.$emit('close','change')
-                    }else if(this.awardItem && this.awardItem.awardsFlag == 'mother_crush_task'){
-                        this.$emit('close','showReceiveMedal')
-                    }else if(curTaskList && curTaskList.parentTask.taskStatus == 0){
-                        this.$emit('close','showMedalAnimate')
-                    }
-                }
+        if(this.masterTask){
+            let index = this.awardItem && this.awardItem.index,
+                curTaskList = this.crushTaskList.allTask[index]
+            if(this.awardItem && this.awardItem.showMedalImg){
+                this.$emit('close','change')
+            }else if(this.awardItem && this.awardItem.awardsFlag == 'mother_crush_task'){
+                this.$emit('close','showReceiveMedal')
+            }else if(curTaskList && curTaskList.parentTask.taskStatus == 0){
+                this.$emit('close','showMedalAnimate')
+            }
+            if(this.newUserTaskFinish){
+              this.$emit('close','reset')
+            }
+        }
+  
 				this.$emit('close')
 			}
+    },
+    mounted() {
+      console.warn(this.newUserTaskFinish)
 		}
 	}
 </script>
@@ -248,4 +265,78 @@
             }
         }
     }
+    .new-user-taks-toask {
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 10;
+  .mask {
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 1;
+    background: rgba(0,0,0,.7);
+  }
+  .rotate {
+    position: absolute;
+    left: 50%;
+    top: 0%;
+    z-index: 2;
+    width: 130%;
+    margin-left: -65%;
+    animation: rotate 2s linear infinite;
+  }
+  .content {
+    position: absolute;
+    left: 50%;
+    top: 40%;
+    z-index: 3;
+    width: 4.8rem;
+    height: 5.3rem;
+    background: url(./img/toast_bg.png) no-repeat 0 0;
+    background-size: 100% 100%;
+    transform: translate(-50%, -50%);
+    overflow: hidden;
+    .title {
+      margin: .59rem auto .56rem;
+      width: 3.64rem;
+      height: .36rem;
+      display: block;
+    }
+    .price {
+      margin: 0 auto .2rem;
+      width: 1.49rem;
+      height: 1.49rem;
+      display: block;
+    }
+    .num {
+      text-align: center;
+      font-size: .26rem;
+      color: #fff;
+    }
+    .btn {
+      margin: .66rem auto 0;
+      width: 2.4rem;
+      height: .62rem;
+      background: #FFCF52;
+      color: #A34C00;
+      text-align: center;
+      line-height: .62rem;
+      border-radius: 5px;
+    }
+  }
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0)
+  }
+  100% {
+    transform: rotate(360deg)
+  }
+}
 </style>
