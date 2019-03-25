876a73986d26a706643d73e71f19f5184046f8eb
彭猛 <pengmeng@beeplay123.com>
2019-03-25 15:00:43 +0800

    gametype

--- src/views/wj/App.vue
+++ src/views/wj/App.vue
@@ -158,6 +158,7 @@ export default {
         cjTaskItems: null,
         isFighur: false,
         timer1: null,
+        cGameType: null,
         sdkBdWap: ['100039','100040','100041','100042','100045','100046',
             '100001','100022','100023','100026','100028','100027','100029','100035','100036','100038', '100006']
       }
@@ -165,7 +166,7 @@ export default {
   mounted() {
     this.curChannel = localStorage.getItem('APP_CHANNEL') ? localStorage.getItem('APP_CHANNEL'):this.getUrlParam('channel')
     this.curToken = localStorage.getItem('ACCESS_TOKEN') ? localStorage.getItem('ACCESS_TOKEN'):this.getUrlParam('token')
-
+    this.cGameType = this.getUrlParam('gameType')
     let cururl = window.location.href
     this.curlink = this.getUrlParam('wf_cur_link')
     if(this.curChannel && this.curChannel.indexOf('100') != -1) {
@@ -303,7 +304,7 @@ export default {
       this.axios.post('//platform-api.beeplay123.com/task/api/usertask/platTaskByBatch', {
         value: localStorage.getItem('wj_gameType')+"-achievementTask",
         from: "sdk",
-        gameType: localStorage.getItem('wj_gameType') || ''
+        gameType: this.cGameType || ''
       },{
             headers: {
                 'App-Channel': this.curChannel,
@@ -317,7 +318,7 @@ export default {
       this.axios.post('//platform-api.beeplay123.com/task/api/usertask/platTaskByBatch', {
         value: localStorage.getItem('wj_gameType')+"-dayTask",
         from: "sdk",
-        gameType: localStorage.getItem('wj_gameType') || ''
+        gameType: this.cGameType || ''
       },{
             headers: {
                 'App-Channel': this.curChannel,
