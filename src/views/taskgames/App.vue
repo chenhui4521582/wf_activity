a6cf47f703d5feb9ea5d879ca694719d3f832783
彭猛 <pengmeng@beeplay123.com>
2019-03-26 14:59:30 +0800

    tiaozhuan

--- src/views/taskgames/App.vue
+++ src/views/taskgames/App.vue
@@ -428,18 +428,13 @@
                     return;
                 }
 
-                // // 球酷
-                // if (url && url.indexOf('databiger-h5') != -1) {
-                //     let gameUrl = this.trimStr(url) + '?channel=' + this.channel + '&token=' + this.token
-                //     return parent.location.href = gameUrl
-                // }
-                // //其他外接游戏external=1
-                // if (url && url.indexOf('external=1') != -1) {
-                //     let gameUrl = this.trimStr(url) + '&channel=' + this.channel + '&token=' + this.token + '&gurl=' + url.split('?')[0] + '&pf=bdWap';
-                //     return parent.location.href = gameUrl
-                // }
-                // parent.location.href = 'https://wap.beeplay123.com' + url + '?channel=' + this.channel + '&token=' + this.token;
-                parent.location.href=common.jumpToGameUrl({url:url})
+                
+                //其他外接游戏external=1
+                if (url && url.indexOf('external=1') != -1) {
+                    let gameUrl = this.trimStr(url) + '&channel=' + this.channel + '&token=' + this.token + '&gurl=' + url.split('?')[0] + '&pf=bdWap';
+                    return parent.location.href = gameUrl
+                }
+                parent.location.href = 'https://wap.beeplay123.com' + url + '?channel=' + this.channel + '&token=' + this.token;
             }, 500)
 
             
