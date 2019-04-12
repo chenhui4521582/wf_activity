4f8774085aab7431407e05e6260d0c01b67f5ab0
chenhuihui <461822621@qq.com>
2019-04-12 16:45:32 +0800

    踏春寻宝  埋点

--- src/views/taskgames/component/boxDialog.vue
+++ src/views/taskgames/component/boxDialog.vue
@@ -30,10 +30,12 @@ export default {
   methods:{
     close () {
       this.$emit('closeBoxDialog')
-    },
+	  GLOBALS.marchSetsPoint('A_H5PT0075000699')
+	},
     go () {
       let channel = GLOBALS.getUrlParam('channel') || localStorage.getItem('APP_CHANNEL')
       let token = GLOBALS.getUrlParam('token') || localStorage.getItem('ACCESS_TOKEN')
+	  GLOBALS.marchSetsPoint('A_H5PT0075000698')
       window.location.href = `https://wap.beeplay123.com/SpringOuting?channel=${channel}&token=${token}`
     }
   }
