0ed073b3ae5dfb946b499dc219e974319ab06e5b
tuyican <waittutu@163.com>
2019-05-22 17:27:27 +0800

    update 添加注释

--- src/views/billshop/utils/api.js
+++ src/views/billshop/utils/api.js
@@ -1,6 +1,6 @@
 import $axios from "../http"
 /**
- * @description 获取话费券商城列表
+ * @description 获取话费券商城列表 新的接口
  */
 export const billList = async ()=>{
     const {data} = await $axios.post('//ops-api.beeplay123.com/ops/api/exchangeMall/mainNew')
