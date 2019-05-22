5b081f6aac72ad2dc96e9280834b3c8c7175ca3c
tuyican <waittutu@163.com>
2019-05-22 17:24:31 +0800

    update 话费列表接口

--- src/views/billshop/utils/api.js
+++ src/views/billshop/utils/api.js
@@ -3,7 +3,7 @@ import $axios from "../http"
  * @description 获取话费券商城列表
  */
 export const billList = async ()=>{
-    const {data} = await $axios.post('//ops-api.beeplay123.com/ops/api/exchangeMall/main')
+    const {data} = await $axios.post('//ops-api.beeplay123.com/ops/api/exchangeMall/mainNew')
     return data
 }
 /**
