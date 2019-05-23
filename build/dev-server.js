41d2518cf53b2581f6dbaa7e03245421e13f39b8
yangwenlan <yangwenlan@patsnap.com>
2019-05-23 11:28:38 +0800

    fix activity dev slow bug

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,92 +1,117 @@
-require('./check-versions')()
+require("./check-versions")();
+var ArgumentParser = require("argparse").ArgumentParser;
 
-var config = require('../config')
+var parser = new ArgumentParser({
+    addHelp: true,
+    description: "add target"
+});
+
+parser.addArgument(["-t", "--target"], {
+    help: "target activity"
+});
+
+const args = parser.parseArgs();
+
+var config = require("../config");
 if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
+    process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV);
 }
 
-var opn = require('opn')
-var path = require('path')
-var express = require('express')
-var webpack = require('webpack')
-var proxyMiddleware = require('http-proxy-middleware')
-var webpackConfig = process.env.NODE_ENV === 'testing'
-  ? require('./webpack.prod.conf')
-  : require('./webpack.dev.conf')
+var opn = require("opn");
+var path = require("path");
+var express = require("express");
+var webpack = require("webpack");
+var proxyMiddleware = require("http-proxy-middleware");
+var webpackConfig =
+    process.env.NODE_ENV === "testing"
+        ? require("./webpack.prod.conf")
+        : require("./webpack.dev.conf");
 
 // default port where dev server listens for incoming traffic
-var port = process.env.PORT || config.dev.port
+var port = process.env.PORT || config.dev.port;
 // automatically open browser, if not set will be false
-var autoOpenBrowser = !!config.dev.autoOpenBrowser
+var autoOpenBrowser = !!config.dev.autoOpenBrowser;
 // Define HTTP proxies to your custom API backend
 // https://github.com/chimurai/http-proxy-middleware
-var proxyTable = config.dev.proxyTable
+var proxyTable = config.dev.proxyTable;
 
-var app = express()
-var compiler = webpack(webpackConfig)
+var app = express();
 
-var devMiddleware = require('webpack-dev-middleware')(compiler, {
-  publicPath: webpackConfig.output.publicPath,
-  quiet: true
-})
+//如果有指定编译目录，那就修改入口
+if(args.target){
+  var entry  = webpackConfig.entry[args.target];
+  webpackConfig.entry = {};
+  webpackConfig.entry[args.target] = entry;
+}
 
-var hotMiddleware = require('webpack-hot-middleware')(compiler, {
-  log: () => {},
-  heartbeat: 2000
-})
+var compiler = webpack(webpackConfig);
+
+var devMiddleware = require("webpack-dev-middleware")(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    quiet: true
+});
+
+var hotMiddleware = require("webpack-hot-middleware")(compiler, {
+    log: () => {},
+    heartbeat: 2000
+});
 // force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', function (compilation) {
-  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
-    cb()
-  })
-})
+compiler.plugin("compilation", function(compilation) {
+    compilation.plugin("html-webpack-plugin-after-emit", function(data, cb) {
+        hotMiddleware.publish({ action: "reload" });
+        cb();
+    });
+});
 
 // proxy api requests
-Object.keys(proxyTable).forEach(function (context) {
-  var options = proxyTable[context]
-  if (typeof options === 'string') {
-    options = { target: options }
-  }
-  app.use(proxyMiddleware(options.filter || context, options))
-})
+Object.keys(proxyTable).forEach(function(context) {
+    console.log("context:" + context);
+    var options = proxyTable[context];
+    if (typeof options === "string") {
+        options = { target: options };
+    }
+    app.use(proxyMiddleware(options.filter || context, options));
+});
 
 // handle fallback for HTML5 history API
-app.use(require('connect-history-api-fallback')())
+app.use(require("connect-history-api-fallback")());
 
 // serve webpack bundle output
-app.use(devMiddleware)
+app.use(devMiddleware);
 
 // enable hot-reload and state-preserving
 // compilation error display
-app.use(hotMiddleware)
+app.use(hotMiddleware);
 
 // serve pure static assets
-var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
-app.use(staticPath, express.static('./static'))
+var staticPath = path.posix.join(
+    config.dev.assetsPublicPath,
+    config.dev.assetsSubDirectory
+);
+app.use(staticPath, express.static("./static"));
 
-var uri = 'http://localhost:' + port
+var uri = "http://localhost:" + port;
 
-var _resolve
+var _resolve;
 var readyPromise = new Promise(resolve => {
-  _resolve = resolve
-})
+    _resolve = resolve;
+});
 
-console.log('> Starting dev server...')
+console.log("> Starting dev server...");
 devMiddleware.waitUntilValid(() => {
-  console.log('> Listening at ' + uri + '\n')
-  // when env is testing, don't need open it
-  if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
-    opn(uri)
-  }
-  _resolve()
-})
+    console.log("> Listening at " + uri + "\n");
+    // when env is testing, don't need open it
+    if (autoOpenBrowser && process.env.NODE_ENV !== "testing") {
+        opn(uri);
+    }
+    _resolve();
+});
 
-var server = app.listen(port)
+var server = app.listen(port);
 
 module.exports = {
-  ready: readyPromise,
-  close: () => {
-    server.close()
-  }
-}
+    ready: readyPromise,
+    close: () => {
+        server.close();
+    }
+};
