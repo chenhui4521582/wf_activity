625ef81591fda7bed29aa461877d7d193dae2892
朱允昌 <zyc925301@163.com>
2019-05-24 18:08:07 +0800

    bugfixed

--- src/views/taskgames/component/tabBox.vue
+++ src/views/taskgames/component/tabBox.vue
@@ -114,8 +114,52 @@ export default {
       this.confirm = false
       let linkUrl = this.selectedActivity.linkUrl
       if (linkUrl.startsWith('/') || linkUrl.includes('external=1')) {
-        GLOBALS.jumpOutsideGame(linkUrl)
+        this.jumpToGame({ url: linkUrl })
       }
+    },
+    jumpToGame (item) {
+      let token = localStorage.getItem('OPEN_ACCESS_TOKEN')
+      let channel = localStorage.getItem('APP_CHANNEL')
+      if (item && item.url.indexOf('external=1') != -1) {
+        if (item.url.includes('?external=1')) {
+          let url =
+            this.trimStr(item.url) +
+            '&channel=' +
+            channel +
+            '&token=' +
+            token +
+            '&gurl=' +
+            item.url.split('?')[0] +
+            '&pf=' + window.linkUrl.getBackUrlFlag(channel)
+          parent.location.href = url
+        } else {
+          let url =
+            this.trimStr(item.url) +
+            '&channel=' +
+            channel +
+            '&token=' +
+            token +
+            '&gurl=' +
+            base64url.encode(item.url.replace('?external=1', '').replace('&external=1', '')) +
+            '&pf=' + window.linkUrl.getBackUrlFlag(channel)
+          parent.location.href = url
+        }
+        return
+      }
+      if (item && item.url.indexOf('databiger-h5') != -1) {
+        let url =
+          this.trimStr(item.url) +
+          '?channel=' +
+          channel +
+          '&token=' +
+          localStorage.getItem('ACCESS_TOKEN')
+        parent.location.href = url
+        return
+      }
+      parent.location.href =
+        item.url +
+        '?channel=' +
+        channel
     }
   },
   computed: {
