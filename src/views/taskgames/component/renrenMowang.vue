9f8666fd10a6a3e3527f4b4f7d7c728fe51ad491
朱允昌 <zyc925301@163.com>
2019-05-31 16:44:39 +0800

    change

---
+++ src/views/taskgames/component/renrenMowang.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    
+  </section>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {
+
+  },
+  computed: {
+
+  },
+  watch: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  mounted () {
+
+  },
+  destroyed () {
+
+  }
+}
+</script>
+
+<style rel="stylesheet/less" lang="less" scoped>
+
+</style>
