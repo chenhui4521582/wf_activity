7a5c4f3a7cb315ed9dfa36cd215c955631f12feb
彭猛 <pengmeng@beeplay123.com>
2019-07-09 15:00:49 +0800

    1

---
+++ src/views/eggs/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="eggs-container">
+		sssssssssdfdsf
+	</div>
+</template>
+<script type="text/javascript">
+	export default {
+
+	}
+</script>
+<style lang="less" scoped></style>
