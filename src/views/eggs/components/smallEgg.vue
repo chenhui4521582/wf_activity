a7b9236d791cb2982833c1174c4ce944cff346d2
彭猛 <pengmeng@beeplay123.com>
2019-07-09 17:40:15 +0800

    eggs

---
+++ src/views/eggs/components/smallEgg.vue
@@ -0,0 +1,72 @@
+<template>
+	<div class="small-egg" :class="isBigStatus?'s-big-egg':''">
+		<img :src="sEggrc" class="c-egg">
+		<span class="s-text">200锤</span>
+	</div>
+</template>
+<script type="text/javascript">
+	export default {
+		data() {
+			return {}
+		},
+		props: {
+			sColor: {
+				default: ''
+			},
+			isBigStatus: {
+				default: false
+			}
+		},
+		computed: {
+			sEggrc() {
+				return require(`../pages/images/index/${this.sColor}-egg.png`)
+			}
+		},
+		mounted() {
+			console.log(this.sColor)
+		}
+	}
+</script>
+<style lang="less" scoped>
+	.small-egg {
+		width: 0.99rem;
+		font-size: 0.2rem;
+		position: relative;
+		&.s-big-egg {
+			width: 1.41rem;
+			height: 1.34rem;
+			.c-egg{
+				width: 1.41rem;
+				height: 1.34rem;
+			}
+			.s-text {
+				width: 1.01rem;
+				height: 0.49rem;
+				line-height: 0.49rem;
+				border-radius: 0.24rem;
+				font-size: 0.32rem;
+				margin-left: -0.5rem;
+			}		
+		}
+	}
+	.c-egg {
+		width: 0.99rem;
+		height: 0.89rem;
+
+	}
+	.s-text {
+		display: block;
+		width: 0.62rem;
+		height: 0.3rem;
+		background:rgba(248,185,24,1);
+		border-radius:0.15rem;
+		font-size:0.2rem;
+		font-weight:bold;
+		color:rgba(154,80,21,1);
+		text-align: center;
+		position: absolute;
+		left: 50%;
+		bottom: 0;
+		margin-left: -0.28rem;
+	}
+</style>
--- src/views/eggs/pages/index.vue
+++ src/views/eggs/pages/index.vue
@@ -1,11 +1,58 @@
 <template>
 	<div class="eggs-container">
-		sssssssssdfdsf
+		<div class="header1">
+			<img src="./images/index/header-title.png" class="header-title">
+			<h4 class="e-time">截止时间：7月7日23:59:59</h4>
+			<img src="./images/index/back.png" class="e-back">
+		</div>
+		<div class="header2"></div>
+		<div class="header3">
+			<a href="javascript:" class="btn-hit">消耗锤子砸彩蛋</a>
+			<div class="e-items">
+				<ul>
+					<li class="">
+						<small-egg sColor="green"></small-egg>
+					</li>
+					<!-- <li>
+						<small-egg sColor="green"></small-egg>
+					</li>
+					<li>
+						<small-egg sColor="green"></small-egg>
+					</li>
+					<li>
+						<small-egg sColor="green"></small-egg>
+					</li>
+					<li>
+						<small-egg sColor="green"></small-egg>
+					</li> -->
+					<!--<li>
+						<small-egg sColor="purplish"></small-egg>
+					</li>
+					 <li>
+						<small-egg sColor="red"></small-egg>
+					</li>
+					<li>
+						<small-egg sColor="blue"></small-egg>
+					</li> -->
+					<!-- <li>
+						<small-egg sColor="big" isBigStatus="true"></small-egg>
+					</li> -->
+				</ul>
+			</div>
+		</div>
 	</div>
 </template>
 <script type="text/javascript">
 	export default {
-
+		data() {
+			return {
+			}
+		},
+		components: {
+			smallEgg: () => import('../components/smallEgg')
+		},
 	}
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+	@import "./css/index.less";
+</style>
