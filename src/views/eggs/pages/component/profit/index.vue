d0a8c536e82edc1d56fdb70608d16502b521e455
朱允昌 <zyc925301@163.com>
2019-07-16 15:00:01 +0800

    change

--- src/views/eggs/pages/component/profit/index.vue
+++ src/views/eggs/pages/component/profit/index.vue
@@ -75,7 +75,24 @@
       </div>
       <div class="profit-footer">仅30名及以内有奖励</div>
     </div>
-    <loading v-if="isLoading"></loading>
+    <div class="loading-wrap" v-if="isLoading">
+      <div class="container">
+        <div class="spinner">
+          <div class="bar1"></div>
+          <div class="bar2"></div>
+          <div class="bar3"></div>
+          <div class="bar4"></div>
+          <div class="bar5"></div>
+          <div class="bar6"></div>
+          <div class="bar7"></div>
+          <div class="bar8"></div>
+          <div class="bar9"></div>
+          <div class="bar10"></div>
+          <div class="bar11"></div>
+          <div class="bar12"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script type="text/javascript">
@@ -103,7 +120,6 @@ export default {
     }
   },
   components: {
-    loading: () => import('../../../../../components/common/loading.vue')
   },
   mounted () {
     this.getRankList()
@@ -117,7 +133,7 @@ export default {
     async getRankList () {
       this.isLoading = true
       const { code, data } = await rankList()
-      if (code == 200) {
+      if (code === 200) {
         this.profitData = data
         if (this.profitData.length > 6) {
           this.lastThreeData = this.profitData.slice(6)
@@ -134,7 +150,7 @@ export default {
     },
     async getActivityInfo () {
       const { code, data } = await activityInfo()
-      if (code == 200) {
+      if (code === 200) {
         this.endTime = data.endTime
         this.countDown(data.countdown)
       }
@@ -177,4 +193,96 @@ export default {
 </script>
 <style lang="less" scoped>
 @import "./index.less";
+.loading-wrap {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 15;
+}
+.container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100px;
+  height: 100px;
+  border-radius: 10%;
+}
+.spinner {
+  height: 40%;
+  width: 40%;
+  position: relative;
+  margin: 0 auto;
+}
+.spinner div {
+  width: 10%;
+  height: 26%;
+  background-color: white;
+  position: absolute;
+  left: 44.5%;
+  top: 37%;
+  opacity: 0;
+  border-radius: 30%;
+  animation: fade 1s linear infinite;
+}
+.spinner div.bar1 {
+  transform: rotate(0deg) translate(0, -142%);
+  animation-delay: 0s;
+}
+.spinner div.bar2 {
+  transform: rotate(30deg) translate(0, -142%);
+  animation-delay: -0.9167s;
+}
+.spinner div.bar3 {
+  transform: rotate(60deg) translate(0, -142%);
+  animation-delay: -0.833s;
+}
+.spinner div.bar4 {
+  transform: rotate(90deg) translate(0, -142%);
+  animation-delay: -0.75s;
+}
+.spinner div.bar5 {
+  transform: rotate(120deg) translate(0, -142%);
+  animation-delay: -0.667s;
+}
+.spinner div.bar6 {
+  transform: rotate(150deg) translate(0, -142%);
+  animation-delay: -0.5833s;
+}
+.spinner div.bar7 {
+  transform: rotate(180deg) translate(0, -142%);
+  animation-delay: -0.5s;
+}
+.spinner div.bar8 {
+  transform: rotate(210deg) translate(0, -142%);
+  animation-delay: -0.41667s;
+}
+.spinner div.bar9 {
+  transform: rotate(240deg) translate(0, -142%);
+  animation-delay: -0.333s;
+}
+.spinner div.bar10 {
+  transform: rotate(270deg) translate(0, -142%);
+  animation-delay: -0.25s;
+}
+.spinner div.bar11 {
+  transform: rotate(300deg) translate(0, -142%);
+  animation-delay: -0.1667s;
+}
+.spinner div.bar12 {
+  transform: rotate(330deg) translate(0, -142%);
+  animation-delay: -0.0833s;
+}
+
+@keyframes fade {
+  from {
+    opacity: 0.8;
+  }
+  to {
+    opacity: 0.25;
+  }
+}
 </style>
