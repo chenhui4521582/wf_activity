2ed9cac60a966024398e8b223031032b2eaf66b0
毛佳 <maojia@beeplay123.com>
2019-07-23 20:13:33 +0800

    话费券商城迭代

--- src/views/billshop/pages/detalis.vue
+++ src/views/billshop/pages/detalis.vue
@@ -37,7 +37,7 @@
             <span class="item-number-title" v-if="currentItem.allUsersTodayAvailableQuota == null && currentItem.currentUserTodayAvailableQuota == null">（剩余库存充足）</span>
             <span class="item-number-title" v-else>（剩余库存: {{residueNumber}}）</span>
             <div class="item-number-add">
-              <field v-model="specNumber" :disabled="currentItem.phyAwardsId === 232" :store-max="currentItem.allUsersTodayAvailableQuota"></field>
+              <field v-model="specNumber" :disabled="currentItem.phyAwardsId === 232" :store-max="currentItem.allUsersTodayAvailableQuota" :buyone="buyone([currentItem])"></field>
             </div>
           </div>
         </div>
