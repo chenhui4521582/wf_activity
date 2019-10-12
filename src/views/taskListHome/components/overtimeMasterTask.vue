1d529f8c87ef61abdb43a316434406ed10f65610
朱允昌 <zhuyunchang@beeplay123.com>
2019-10-12 16:09:24 +0800

    fixed

--- src/views/taskListHome/components/overtimeMasterTask.vue
+++ src/views/taskListHome/components/overtimeMasterTask.vue
@@ -70,7 +70,7 @@ export default {
       }, 1000)
     },
     goOvertimeDetail () {
-      WapCall.openGame('/activities/overtimeTask.html', '&gametype=' + this.gameType)
+      WapCall.openGame('/activities/overtimeTask.html', null, '&gametype=' + this.gameType)
     }
   }
 }
