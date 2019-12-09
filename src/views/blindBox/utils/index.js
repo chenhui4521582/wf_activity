f1c21c24ff4c2a0f8d8fab54b1ea1a6821a03cb9
朱允昌 <zhuyunchang@beeplay123.com>
2019-12-09 19:07:50 +0800

    fixed 地址

--- src/views/blindBox/utils/index.js
+++ src/views/blindBox/utils/index.js
@@ -4,10 +4,10 @@ export class Pay {
   // url 跳转支付页面的url
   // JDD_PARAM payment 缓存支付信息
   static toPay ({ originDeffer = location.href, payInfo }) {
-    let url = 'https://wap.beeplaying.com/xmWap/#/payment/paymentlist'
-    let payStorage = 'payment'
-    localStorage.setItem(payStorage, JSON.stringify(payInfo))
-    localStorage.setItem('originDeffer', originDeffer)
+    let url = `https://wap.beeplaying.com/xmWap/#/payment/paymentlist?from=${encodeURIComponent(
+      originDeffer
+    )}`
+    localStorage.setItem('payment', JSON.stringify(payInfo))
     location.replace(url)
   }
 }
