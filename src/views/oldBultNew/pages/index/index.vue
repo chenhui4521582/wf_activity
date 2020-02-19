01e9f0c41a88d02aee941d54fc6961691e4bcf07
Shawn <yezi@17bixin.cn>
2020-02-19 13:26:18 +0800

    老带新

---
+++ src/views/oldBultNew/pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="main">
+    dfsafdsasdf
+  </main>
+</template>
+
+<script>
+export default {
+  mounted () {
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .main {
+    background: url('../../assets/bg.png') no-repeat;
+    background-size: 100% 100%;
+    height: 100vh;
+  }
+</style>
