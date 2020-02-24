ba6daa6622fca75ddb93b8246d4aee833cdb081b
Shawn <yezi@17bixin.cn>
2020-02-24 15:32:20 +0800

    update

--- src/views/blindBox/apis/score.js
+++ src/views/blindBox/apis/score.js
@@ -2,3 +2,7 @@ import client from '../client'
 
 // 是否签到
 export const SignInState = () => client.post(`@box/score/sign-in-state`)
+
+// 积分首页信息
+export const Index = () => client.post(`@box/score/index`)
+
--- src/views/blindBox/pages/score/components/my-score/index.vue
+++ src/views/blindBox/pages/score/components/my-score/index.vue
@@ -1,17 +1,111 @@
 <template>
-  <section class="wrapper">
-    <p class="title">我的积分</p>
-  </section>
+  <article>
+    <section class="wrapper">
+      <div class="float left">
+        积分攻略
+      </div>
+      <div class="float right">
+        兑换记录
+      </div>
+      <p class="title">我的积分</p>
+      <p class="count">
+        <img src="./assets/score.png"
+          alt="">1290
+      </p>
+      <p class="tip">连续签到得更多积分</p>
+    </section>
+    <img :src="arrow"
+      @click="handleArrow"
+      class="arrow">
+  </article>
 </template>
 
 <script>
-export default {
+import { SignInState, Index } from '../../../../apis/score'
 
+export default {
+  data () {
+    return {
+      isOpen: true
+    }
+  },
+  methods: {
+    handleArrow () {
+      this.isOpen = !this.isOpen
+    }
+  },
+  computed: {
+    arrow () {
+      if (this.isOpen) return require('./assets/close.png')
+      return require('./assets/open.png')
+    }
+  },
+  async mounted () {
+    await Index()
+    const { data: { data } } = await SignInState()
+    if (data) this.isOpen = false
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.arrow {
+  width: 1rem;
+  display: block;
+  margin: 0 auto;
+  height: 0.3rem;
+}
 .wrapper {
-  background: url('./assets/bg.png') no-repeat 100% 100%;
+  background: url("./assets/bg.png") no-repeat 100% 100%;
+  padding-bottom: 0.2rem;
+  position: relative;
+  .float {
+    width: 1.2rem;
+    line-height: 0.44rem;
+    text-align: center;
+    font-size: 0.22rem;
+    color: #fff;
+    position: absolute;
+    top: 0.3rem;
+    &.left {
+      background: rgba(255, 255, 255, 0.4);
+      border-radius: 0 0.22rem 0.22rem 0;
+      left: 0;
+    }
+    &.right {
+      background: rgba(222, 192, 114, 0.6);
+      border-radius: 0.22rem 0 0 0.22rem;
+      right: 0;
+    }
+  }
+  .record {
+    position: absolute;
+    top: 0.3rem;
+    right: 0;
+  }
+  .title {
+    padding: 0.31rem 0 0.23rem;
+    text-align: center;
+    font-size: 0.28rem;
+    color: #fff;
+  }
+  .count {
+    display: flex;
+    justify-content: center;
+    align-items: baseline;
+    font-size: 0.6rem;
+    color: #fde9b3;
+    font-weight: bold;
+    img {
+      width: 0.37rem;
+      margin-right: 0.1rem;
+    }
+  }
+  .tip {
+    padding: 0.25rem 0 0.2rem;
+    text-align: center;
+    color: #fff;
+    font-size: 0.2rem;
+  }
 }
 </style>
