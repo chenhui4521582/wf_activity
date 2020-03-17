cad4af0b49a75e1edb30f6062d08b3120f3c0485
yangwenlan <yangwenlan@patsnap.com>
2020-03-17 18:04:59 +0800

    optimize

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,6 +4,11 @@ var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
 var webpack = require("webpack")
 
+// 用happyPack发现速度没什么提升
+var os = require('os');
+var HappyPack = require('happypack');
+var happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length});
+
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
@@ -38,32 +43,45 @@ module.exports = {
     new webpack.ProvidePlugin({
         $: "jquery",
         jQuery: "jquery"
+    }),
+    new HappyPack({
+      id: 'jsx',
+      loaders: ['babel-loader?cacheDirectory=true'],
+      threadPool: happyThreadPool,
+    }),
+    new HappyPack({
+      id: 'styles',
+      loaders: [ 'style-loader', 'css-loader', 'less-loader' ],
+      threadPool: happyThreadPool
     })
  ],
   module: {
+    loaders: [
+      {
+        test: /\.js[x]?$/,
+        include: [resolve('src')],
+        exclude: /node_modules/,
+        loader: 'happypack/loader?id=happybabel'
+      }
+    ],
     rules: [
-      // {
-      //   test: /\.(js|vue)$/,
-      //   loader: 'eslint-loader',
-      //   enforce: 'pre',
-      //   include: [resolve('src'), resolve('test')],
-      //   options: {
-      //     formatter: require('eslint-friendly-formatter')
-      //   }
-      // },
       {
         test: /\.vue$/,
         loader: 'vue-loader',
-        options: vueLoaderConfig
+        options: vueLoaderConfig,
+        include: [resolve('src')],
       },
       {
         test: /\.less$/,
-        loader: 'style!css!less'
+        //loader: 'style!css!less'
+        use: 'happypack/loader?id=styles'
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        use: 'happypack/loader?id=jsx',
+        //loader: 'babel-loader?cacheDirectory=true',
+        exclude: /node_modules/,
+        include: [resolve('src'), resolve('test')],
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
@@ -73,14 +91,14 @@ module.exports = {
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
-      {
-        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('media/[name].[hash:7].[ext]')
-        }
-      },
+      // {
+      //   test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+      //   loader: 'url-loader',
+      //   options: {
+      //     limit: 10000,
+      //     name: utils.assetsPath('media/[name].[hash:7].[ext]')
+      //   }
+      // },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
