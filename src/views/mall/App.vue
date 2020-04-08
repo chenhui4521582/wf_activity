963ff20c3369f958918a1220978b83a4ef2d9850
chenhuihui <461822621@qq.com>
2020-04-08 15:19:54 +0800

    修改

--- src/views/mall/App.vue
+++ src/views/mall/App.vue
@@ -6,3 +6,7 @@ export default {
   name: 'index'
 }
 </script>
+<style scoped type="less">
+@import '../../common/css/base.css';
+@import url('../../../static/iconfont/iconfont.css');
+</style>
---
+++ src/views/mall/views/productList/productList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="product-list">
+    <Header title="话费券当钱花"/>
+    <div class="banner">
+      <img src="./img/banner.png" alt="">
+    </div>
+    <div class="nav">
+
+    </div>
+    <div class="list">
+      <list-item />
+    </div>
+  </div>
+</template>
+<script>
+import ListItem from './components/listItem'
+export default {
+  name: 'productList',
+  data: () => ({
+    nav: ['']
+  }),
+  components: {
+    ListItem
+  }
+}
+</script>
+<style lang="less" scoped>
+img {
+  width: 100%;
+  height: 100%;
+  vertical-align: top;
+}
+</style>
