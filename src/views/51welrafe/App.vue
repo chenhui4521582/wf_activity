ea95bbfbd450feb20cb955d2090eb1ffe086f891
chenhuihui <461822621@qq.com>
2020-04-23 11:13:29 +0800

    51福利游乐场

---
+++ src/views/51welrafe/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="welrafe">
+
+  </div>
+</template>
+<script>
+import Rule from './components/rule'
+import Services from './services/services'
+import utils from './components/utils'
+import _get from 'lodash.get'
+export default {
+  name: 'welrafe',
+  data: ()=>({
+  }),
+  components: {
+    Popup,
+    Rule,
+  },
+  computed: {
+  },
+  methods: {},
+  mounted() {
+    this.init()
+  }
+}
+</script>
+<style scoped lang="less">
+@import '../../common/css/base.css';
+@import url('../../../static/iconfont/iconfont.css');
+* {
+  box-sizing: border-box;
+}
+.welrafe {
+  
+}
+</style>
---
+++ src/views/51welrafe/services/services.js
@@ -0,0 +1,76 @@
+import axios from './http'
+/**
+ *  @description  福利游乐场-获取活动信息
+ */
+const getInfo = ( params ) => {
+  let url = '//ops-api.beeplaying.com/ops/api/playground/activity-info'
+  return axios.post(url, params)
+}
+
+/**
+ *  @description  福利游乐场-兑换记录
+ */
+const getExchangeLog = ( params ) => {
+  let url = '//ops-api.beeplaying.com/ops/api/playground/convert-record'
+  return axios.post(url, params)
+}
+
+/**
+ *  @description  福利游乐场-兑换
+ */
+const exchange = ( params ) => {
+  let url = `//ops-api.beeplaying.com/ops/api/playground/convert/${params}`
+  return axios.post(url)
+}
+
+/**
+ *  @description  福利游乐场-排行榜列表
+ */
+const getRanking = ( params ) => {
+  let url = '//ops-api.beeplaying.com/ops/api/playground/rank-list'
+  return axios.post(url, params)
+}
+
+/**
+ *  @description  福利游乐场-进度领取
+ */
+const getProgress = ( params ) => {
+  let url = `//ops-api.beeplaying.com/ops/api/playground/task-receive/${params.type}/${params.sort}`
+  return axios.post(url, params)
+}
+
+/**
+ *  @description  福利游乐场-用户任务进度
+ */
+const getUserProgress = ( params ) => {
+  let url = `//ops-api.beeplaying.com/ops/api/playground/user-progress`
+  return axios.post(url, params)
+}
+
+/**
+ *  @description  福利游乐场-用户获取道具记录
+ */
+const getUserPropLog = ( params ) => {
+  let url = `//ops-api.beeplaying.com/ops/api/playground/user-prop-record`
+  return axios.post(url, params)
+}
+
+/**
+ *  @description  福利游乐场-用户排行信息
+ */
+const getUserRanking = ( params ) => {
+  let url = `//ops-api.beeplaying.com/ops/api/playground/user-rank`
+  return axios.post(url, params)
+}
+
+const services = {
+  getInfo,
+  getExchangeLog,
+  exchange,
+  getRanking,
+  getProgress,
+  getUserProgress,
+  getUserPropLog,
+  getUserRanking
+}
+export default services
