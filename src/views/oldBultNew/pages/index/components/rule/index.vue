53ea4788093383c22175241d18f8de2d1a458dda
chenhuihui <461822621@qq.com>
2020-05-26 11:25:55 +0800

    安卓老带新活动优化

--- src/views/oldBultNew/pages/index/components/rule/index.vue
+++ src/views/oldBultNew/pages/index/components/rule/index.vue
@@ -2,8 +2,9 @@
   <Modal :show="show"
     @onClose="$emit('onClose')">
     <p class="title">活动介绍：</p>
+    
     <p class="content">
-    活动时间：{{begin}}—{{end}}
+    活动时间：{{begin}}—{{end}}<br>
     1.活动期间点击页面下方邀请按钮邀请好友，好友接受邀请后并登录，即可获得1000金叶子奖励；<br>
     2.邀请好友完成平台新手任务，好友获得任务奖励，你可再得2000金叶子奖励；<br>
     3.邀请好友在平台充值，你可得好友充值的金叶子返利，返利比例为1%起。邀请的好友越多，返利比例越高，返利无上限封顶！<br>
@@ -17,14 +18,9 @@
 
   </Modal>
 
-  
-
-
 </template>
-
 <script>
 import Modal from '../../../../components/modal'
-
 export default {
   components: {
     Modal
@@ -50,6 +46,10 @@ export default {
 }
 .content {
   padding: 0.24rem 0.44rem 0.3rem;
+  height: 6rem;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  -webkit-overflow-scrolling: touch;
   font-size: 0.24rem;
   color: #888888;
   text-align: left;
