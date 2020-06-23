8887c6451e9de0711f3660dce5934631db0cca08
朱允昌 <zhuyunchang@beeplay123.com>
2020-06-23 15:02:06 +0800

    fixed

---
+++ src/views/bdMiddlePage/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <div>立即下载领奖</div>
+    <p>用户已绑定手机号登录，奖品即可到账</p>
+    <p>
+      游戏服务由第三方多多玩提供<br />
+      如有问题咨询多多玩客服:
+      <span>
+        youxikefu2020
+      </span>
+    </p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
