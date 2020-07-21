01b4b182d32868b35be89a58f7b9845295660851
毛佳 <maojia@beeplay123.com>
2020-07-21 10:34:46 +0800

    jinbi

--- src/views/coinact/App.vue
+++ src/views/coinact/App.vue
@@ -1,9 +1,35 @@
 <template>
   <section class="coinact">
-    <div class="time" v-if="countdown.time">{{countdown.time.split('分')[0]+'分'}}结束</div>
-    <img src="./images/back.png" alt="" class="back" @click="back">
-    <template v-if="actInfo&&actInfo.state==1">
-    </template>
+    <div class="coinact_bg">
+      <img src="./images/back.png" alt="" class="back" @click="back">
+      <img src="./images/rule.png" alt="" class="rule" @click="back">
+      <div class="time">活动时间:10/11 23:59:50-10/12 23:59:50</div>
+      <div class="total_gain">
+        <div class="item">累计获得：<i>000/300金币</i></div>
+        <div class="item">每支持10000金叶=1个金币（部分游戏不计入）</div>
+      </div>
+      <div class="coin_stage">
+        <div class="item">3</div>
+        <div class="item">下一达成阶段</div>
+      </div>
+      <div class="coin_list">
+        <div class="coin_left">
+          <div class="item" v-for="item in list"
+               :class="{gray:item.status==2,complete:item.status==0,receive:item.status==1}">
+            <div class="price">{{item.price}}元</div>
+            <div class="status">{{status[item.status]}}元</div>
+          </div>
+        </div>
+        <div class="coin_percent" v-for="(item,index) in list" :class="{gray:item.status==0}">{{index+1}}</div>
+        <div class="coin_right">
+          <div class="item" v-for="item in list"
+               :class="{gray:item.status==2,complete:item.status==0,receive:item.status==1}">
+            <div class="price">{{item.price}}元</div>
+            <div class="status">{{status[item.status]}}元</div>
+          </div>
+        </div>
+      </div>
+    </div>
     <com-pop ref="comPop" :awardData="awardData"></com-pop>
   </section>
 </template>
@@ -23,7 +49,27 @@
         countdown: {
           time: ''
         },
-        awardData: null
+        awardData: null,
+        status: ["去完成", "未领取", "已领取"],
+        list: [{
+          price: 1,
+          status: 0
+        }, {
+          price: 11,
+          status: 1
+        }, {
+          price: 111,
+          status: 2
+        }, {
+          price: 1,
+          status: 0
+        }, {
+          price: 1,
+          status: 0
+        }, {
+          price: 1,
+          status: 0
+        }]
       }
     },
     async mounted() {
@@ -98,184 +144,137 @@
 <style lang="less">
   @import "../../common/css/base.css";
 
-  .payeddecoration {
-    height: 100vh;
-    background: #5741D1;
+  .coinact {
+    min-height: 100vh;
     position: relative;
     &:before {
       content: '';
       position: absolute;
       left: 0;
       right: 0;
-      top: 0;
-      height: 2.78rem;
-      background: url("./images/case.png");
+      bottom: 0;
+      height: 2.28rem;
+      background: url("./images/bgline.png");
       background-size: 100% 100%;
       z-index: 1;
     }
-    &:after {
-      content: '';
-      position: absolute;
+    .coinact_bg {
+      position: relative;
       left: 0;
       right: 0;
-      bottom: 0;
-      height: 11.6rem;
+      height: 14.86rem;
       background: url("./images/bg.png");
       background-size: 100% 100%;
     }
     .time {
       position: absolute;
-      top: 1.83rem;
+      top: 3.84rem;
       left: 0;
       right: 0;
       z-index: 2;
       text-align: center;
-      font-size: .24rem;
-      font-weight: bold;
-      color: rgba(255, 255, 255, 1);
+      font-size: .2rem;
+      font-weight: 400;
+      color: rgba(171, 74, 32, 1);
     }
-    .back {
+    .back, .rule {
       position: absolute;
       top: .2rem;
-      left: -.1rem;
-      width: .76rem;
-      height: .5rem;
+      left: .16rem;
+      width: .66rem;
+      height: .66rem;
       z-index: 2;
+      &.rule {
+        top: 1.06rem;
+      }
     }
-    .package1 {
+    .total_gain {
       position: absolute;
-      top: 2.3rem;
-      left: 0;
-      right: 0;
-      margin: auto;
-      width: 6.72rem;
-      height: 3.88rem;
-      background: url("./images/paybg1.png");
-      background-size: 100% 100%;
-      z-index: 2;
+      top: 4.25rem;
+      height: 1rem;
+      left: .5rem;
+      width: 4.3rem;
       display: flex;
-      flex-wrap: wrap;
       flex-direction: column;
-      justify-content: space-between;
       align-items: center;
-      padding: .41rem 0 .23rem;
-      box-sizing: border-box;
+      justify-content: center;
       .item {
-        width: 3.74rem;
-        height: 1.5rem;
-        display: flex;
-        align-items: center;
-        img {
-          width: 1.5rem;
-          height: 1.5rem;
-        }
+        font-size: .18rem;
+        font-weight: 400;
+        color: rgba(160, 83, 13, 1);
+        text-align: center;
         &:nth-child(1) {
-          width: 2.24rem;
-          height: 3rem;
-          flex-direction: column;
-          justify-content: center;
-          img {
-            margin-bottom: .16rem;
-          }
-          .container .content {
-            text-align: center;
+          font-size: .32rem;
+          font-weight: bold;
+          color: rgba(156, 47, 13, 1);
+          margin-bottom: .15rem;
+          i {
+            color: rgba(255, 0, 0, 1);
           }
         }
-        .container {
-          position: relative;
-          width: 2.24rem;
-          height: 1.34rem;
-          .content {
-            font-size: .24rem;
-            font-weight: 800;
-            color: rgba(255, 255, 255, 1);
-            line-height: .3rem;
-            display: flex;
-            justify-content: center;
+      }
+    }
+    .coin_stage {
+      position: absolute;
+      top: 4.25rem;
+      height: 1rem;
+      right: .5rem;
+      width: 1.71rem;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      .item {
+        font-size: .18rem;
+        font-weight: 400;
+        color: rgba(232, 198, 255, 1);
+        text-align: center;
+        &:nth-child(1) {
+          font-size: .48rem;
+          font-weight: bold;
+          color: rgba(255, 253, 250, 1);
+          margin-bottom: .13rem;
+          line-height: 1;
+        }
+      }
+    }
+    .coin_list{
+      display: flex;
+      justify-content: space-between;
+      .coin_left{
+        .item{
+          width: 2.41rem;
+          height: 1.39rem;
+          background: url("./images/left.png");
+          background-size: 100% 100%;
+          font-size:.36rem;
+          font-weight:bold;
+          color:rgba(254,240,91,1);
+          .status{
+            width:1.04rem;
+            height:.44rem;
+            line-height:.44rem;
+            text-align: center;
+            background:rgba(110,26,173,1);
+            border-radius:.22rem;
+            font-size:.24rem;
+            font-weight:400;
+            color:rgba(255,255,255,1);
           }
-          .btn {
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            width: 2.24rem;
-            height: .9rem;
-            background: url("./images/buybtn.png");
-            background-size: 100% 100%;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            font-size: .27rem;
-            font-weight: bold;
-            color: rgba(195, 87, 0, 1);
-            padding-top: .15rem;
-            box-sizing: border-box;
-            &.buyed {
-              background: url("./images/buyed.png");
-              background-size: 100% 100%;
+          &.receive{
+            .status{
+              background:rgba(255,240,225,1);
+              color:rgba(207,54,54,1);
             }
           }
-        }
-      }
-    }
-    .package2 {
-      position: absolute;
-      top: 6.3rem;
-      left: 0;
-      right: 0;
-      margin: auto;
-      width: 6.72rem;
-      height: 5.17rem;
-      background: url("./images/paybg2.png");
-      background-size: 100% 100%;
-      z-index: 2;
-      padding: .39rem 0 .3rem;
-      box-sizing: border-box;
-      .item {
-        padding-left: .25rem;
-        box-sizing: border-box;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        img {
-          width: 1.6rem;
-          height: 1.6rem;
-        }
-        .content {
-          margin-left: .06rem;
-          font-size: .24rem;
-          font-weight: 800;
-          color: rgba(255, 255, 255, 1);
-          line-height: .4rem;
-          display: flex;
-          justify-content: center;
-        }
-        .btn {
-          width: 2.24rem;
-          height: .9rem;
-          background: url("./images/buybtn.png");
-          background-size: 100% 100%;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          font-size: .27rem;
-          font-weight: bold;
-          color: rgba(195, 87, 0, 1);
-          padding-top: .15rem;
-          box-sizing: border-box;
-          &.buyed {
-            background: url("./images/buyed.png");
-            background-size: 100% 100%;
+          &.gray{
+            background: url("./images/l_gray.png");
+            background: 100% 100%;
+            color:rgba(232,232,232,1);
+            .status{
+              background:rgba(110,108,108,1);
+            }
           }
-        }
-      }
-      .pics {
-        width: 6.4rem;
-        margin: .16rem auto 0;
-        display: flex;
-        justify-content: space-around;
-        img {
-          width: 2rem;
-          height: 2.72rem;
         }
       }
     }
