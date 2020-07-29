377b51cf213fd3b1d89a2b82ff5d0c62bfc02888
朱允昌 <zhuyunchang@beeplay123.com>
2020-07-29 15:30:20 +0800

    stage

--- src/views/returnBackUser/App.vue
+++ src/views/returnBackUser/App.vue
@@ -9,6 +9,31 @@
     <div class="problem">
       <img src="./img/problem-icon.png" alt="">
     </div>
+    <article>
+      <section class="cash-out">
+        <section>
+          <p>0</p>
+          <div class="percent">
+            <div class="percent-wrapper">
+              <div class="percent-content"></div>
+            </div>
+            <div class="percent-tips">已获得1.5元</div>
+          </div>
+          <p>10元红包</p>
+        </section>
+        <p>
+          <span>集满10元红包即可提现10元话费</span>
+          <span class="line"></span>
+          <span class="history">
+            领取记录>>
+          </span>
+        </p>
+      </section>
+      <section :class="`btn-wrapper status-${status}`">
+        <button type="button">{{status?'未达提现资格':'立即提现'}}</button>
+      </section>
+      <section>最晚提现时间为 x月x日 23:59:59 逾期作废</section>
+    </article>
   </main>
 </template>
 
@@ -26,7 +51,8 @@ export default {
       isShowPop: false,
       isShowGame: false,
       popType: 'award',
-      awardsInfo: {}
+      awardsInfo: {},
+      status: 1
     }
   },
   mounted () {
@@ -41,6 +67,8 @@ export default {
   width: 100vw;
   min-height: 100vh;
   background: linear-gradient(#b52a2e 9.2rem, #b7292e);
+  padding-top: 0.01rem;
+  box-sizing: border-box;
   .bg {
     position: absolute;
     top: 0;
@@ -55,10 +83,16 @@ export default {
   .back {
     width: 0.66rem;
     height: 0.66rem;
+    font-size: 0;
+    margin-top: 0.25rem;
+    margin-left: 0.16rem;
   }
   .problem {
     width: 0.66rem;
     height: 0.66rem;
+    font-size: 0;
+    margin-top: 0.15rem;
+    margin-left: 0.16rem;
   }
 }
 </style>
