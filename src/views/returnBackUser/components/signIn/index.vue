2932ce8c38fd384060dedb4162942c6dd1a36dac
朱允昌 <zhuyunchang@beeplay123.com>
2020-07-30 14:07:55 +0800

    stage

--- src/views/returnBackUser/components/signIn/index.vue
+++ src/views/returnBackUser/components/signIn/index.vue
@@ -7,9 +7,16 @@
         <span>3天</span>
       </p>
       <ul class="list">
-        <li v-for="item in list">
-          <template v-if="item.day%2">{{item.day}}天</template>
-          <template v-else>{{item.day}}个 <br /><span>话费红包</span> </template>
+        <li v-for="item in list" :class="`type-${item.day}${item.status}`">
+          <p>
+            <template v-if="item.day%2"><span class="fs24">{{item.day}}天</span></template>
+            <template v-else-if="item.status===2">
+              <span class="fs18">红包</span> <br /><span class="fs18">已领取</span>
+            </template>
+            <template v-else>
+              <span class="fs20">{{item.day}}个</span> <br /><span class="fs18">话费红包</span>
+            </template>
+          </p>
         </li>
       </ul>
     </section>
@@ -25,12 +32,12 @@ export default {
   data () {
     return {
       list: [
-        { day: 1, status: 1 },
-        { day: 2, status: 0 },
-        { day: 3, status: 0 },
-        { day: 4, status: 0 },
-        { day: 5, status: 0 },
-        { day: 6, status: 0 }
+        { day: 1, status: 2 },
+        { day: 2, status: 1 },
+        { day: 3, status: 1 },
+        { day: 4, status: 1 },
+        { day: 5, status: 1 },
+        { day: 6, status: 1 }
       ]
     }
   },
@@ -59,7 +66,7 @@ export default {
     margin: auto;
   }
   .sign-in-wrapper {
-    width: 6rem;
+    width: 6.62rem;
     height: 2.36rem;
     margin: 0.1rem auto 0;
     background: url('../../img/sign-bg.png') no-repeat center center;
@@ -72,6 +79,68 @@ export default {
       span {
         color: #dd2930;
         font-weight: bold;
+      }
+    }
+    .list {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      li {
+        width: 0.86rem;
+        height: 1.16rem;
+        white-space: nowrap;
+        font-weight: 400;
+        position: relative;
+        margin: 0.36rem 0.1rem 0;
+        p {
+          position: absolute;
+          bottom: 0.1rem;
+          width: 100%;
+          line-height: 1;
+        }
+        .fs18 {
+          font-size: 0.18rem;
+        }
+        .fs20 {
+          font-weight: bold;
+          font-size: 0.2rem;
+        }
+        .fs24 {
+          font-size: 0.24rem;
+        }
+        background: no-repeat center center;
+        background-size: 100% 100%;
+        &.type-11,
+        &.type-31,
+        &.type-51 {
+          color: #4e4c4c;
+          background-image: url('../../img/odd-grey-bg.png');
+        }
+        &.type-12,
+        &.type-32,
+        &.type-52 {
+          color: #e3292a;
+          background-image: url('../../img/odd-orange-bg.png');
+        }
+        &.type-20,
+        &.type-40,
+        &.type-60 {
+          color: #4e4c4c;
+          background-image: url('../../img/even-grey-bg.png');
+        }
+        &.type-21,
+        &.type-41,
+        &.type-61 {
+          color: #fff;
+          background-image: url('../../img/even-red-bg.png');
+        }
+        &.type-22,
+        &.type-42,
+        &.type-62 {
+          color: #e3292a;
+          background-image: url('../../img/even-orange-bg.png');
+        }
       }
     }
   }
