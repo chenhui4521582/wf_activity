cce4a673255590816f17b955ae8a9fb7f9892c0f
毛佳 <maojia@beeplay123.com>
2020-08-11 16:28:02 +0800

    道具的馈赠

---
+++ src/views/propsgifts/utils/api.js
@@ -0,0 +1,34 @@
+import $axios from '../../../http'
+
+/**
+ * @description 热门帖子列表
+ * @return {Object} data
+ */
+export const getActInfo = async () => {
+  const {data} = await $axios.post('//ops-api.beeplaying.com/ops/api/prop-gift/activity-info')
+  return data
+}
+/**
+ * @description 抽奖
+ * @return {Object} data
+ */
+export const drawPrize = async () => {
+  const {data} = await $axios.post('//ops-api.beeplaying.com/ops/api/prop-gift/draw')
+  return data
+}
+/**
+ * @description 兑换
+ * @return {Object} data
+ */
+export const exchagePrize = async (sort) => {
+  const {data} = await $axios.post(`//ops-api.beeplaying.com/ops/api/prop-gift/exchange/${sort}`)
+  return data
+}
+/**
+ * @description 获得记录
+ * @return {Object} data
+ */
+export const getRecords = async (popType) => {
+  const {data} = await $axios.post(popType==5?'//ops-api.beeplaying.com/ops/api/prop-gift/draw-record':'//ops-api.beeplaying.com/ops/api/prop-gift/exchange-record')
+  return data
+}
