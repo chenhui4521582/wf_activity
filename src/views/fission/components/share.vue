c1f12f6065412c8f03b2db794cdeb9ae46d03f3e
毛佳 <maojia@beeplay123.com>
2020-08-21 17:47:47 +0800

    开宝箱裂变活动

--- src/views/fission/components/share.vue
+++ src/views/fission/components/share.vue
@@ -3,27 +3,31 @@
     <div class="mask" v-if="value"></div>
     <transition name="slide1">
       <div class="share-box" v-if="value">
-        <div class="box">
+        <div class="fission-box">
           <div class="wrap">
             <swiper :options="options" ref="mySwiper">
-              <swiper-slide data="1"><img src="../img/share/pic1.png" alt="" /><div class="bottom"></div></swiper-slide>
-              <swiper-slide data="2"><img src="../img/share/pic2.png" alt="" /><div class="bottom"></div></swiper-slide>
-              <swiper-slide data="3"><img src="../img/share/pic3.png" alt="" /><div class="bottom"></div></swiper-slide>
+              <swiper-slide data="1"><img src="../img/share/pic1.png" alt="" /></swiper-slide>
+              <swiper-slide data="2"><img src="../img/share/pic2.png" alt="" /></swiper-slide>
+              <swiper-slide data="3"><img src="../img/share/pic3.png" alt="" /></swiper-slide>
             </swiper>
           </div>
           <div class="swiper-button-prev" slot="button-prev"></div>
           <div class="swiper-button-next" slot="button-next"></div>
         </div>
-        <div class="bottom">
+        <div class="fission-bottom">
           <div class="info">邀请好友 得开宝箱钥匙</div>
           <div class="list">
-            <div class="item" @click="shareWechat('1')">
-              <img src="../img/wechat1.png" alt="">
-              <p>朋友圈</p>
-            </div>
             <div class="item" @click="shareWechat('0')">
-              <img src="../img/wechat.png" alt="">
-              <p>微信</p>
+              <img src="../img/share/wechat.png" alt="">
+              <p>邀请微信好友</p>
+            </div>
+            <div class="item" @click="shareWechat('1')">
+              <img src="../img/share/wechat1.png" alt="">
+              <p>发送朋友圈</p>
+            </div>
+            <div class="item" @click="shareWechat('1')">
+              <img src="../img/share/link.png" alt="">
+              <p>发送专属链接</p>
             </div>
           </div>
           <div class="close" @click="hide">
@@ -54,8 +58,8 @@ export default {
   data(){
     return {
       options: {
-        slidesPerView: 3,
-        spaceBetween: 15,
+        slidesPerView: 1,
+        spaceBetween: 0,
         centeredSlides: true,
         loop: true,
         navigation: {
@@ -66,7 +70,6 @@ export default {
           slideChangeTransitionEnd: () => {
             let element = document.querySelector('.swiper-slide-active')
             boxVm.$emit('slideChange', element.getAttribute('data'))
-            GLOBALS.marchSetsPoint('A_H5PT0308003732')
           }
         }
       }
@@ -127,7 +130,72 @@ export default {
     right: 0;
     height: 12.8rem;
     z-index: 12;
-    .bottom{
+    .fission-box {
+      .swiper-slide {
+        display: flex;
+        justify-content: flex-start;
+        height: 10.23rem;
+        justify-items: center;
+        align-items: center;
+        img {
+          width: 100%;
+        }
+        .bottom {
+          display: none;
+        }
+      }
+      .swiper-slide-active {
+        position: relative;
+        justify-content: center;
+        img {
+          position: relative;
+          z-index: 1;
+          width: 100%;
+        }
+        .bottom {
+          display: block;
+          position: absolute;
+          left: 50%;
+          top: 2.8rem;
+          transform:  translate(-50%, 0);
+          margin-left: -1.92rem;
+          width: 3.84rem;
+          height: 1.42rem;
+          background: url(../img/bottom.png) no-repeat center center;
+          background-size: 100% 100%;
+          animation: cover infinite 2s;
+        }
+        &.active {
+          img {
+            animation: award 1s 1;
+          }
+        }
+      }
+      .swiper-slide-prev {
+        justify-content: flex-end;
+      }
+      .swiper-slide-next {
+        justify-content: flex-start;
+      }
+      .swiper-button-prev {
+        width: .9rem;
+        height: .9rem;
+        background: url(../img/box-btn.png) no-repeat center center;
+        background-size: 100% 100%;
+        cursor: auto;
+        outline: none;
+      }
+      .swiper-button-next {
+        transform: rotate(180deg);
+        width: .9rem;
+        height: .9rem;
+        background: url(../img/box-btn.png) no-repeat center center;
+        background-size: 100% 100%;
+        cursor: auto;
+        outline: none;
+      }
+    }
+    .fission-bottom{
       height: 3.85rem;
       position: absolute;
       left: 0;
@@ -138,6 +206,7 @@ export default {
       border-radius:.32rem .32rem 0 0;
       padding: .3rem 0 0;
       box-sizing: border-box;
+      z-index: 1;
       .info{
         font-size:.24rem;
         font-weight:400;
@@ -146,18 +215,17 @@ export default {
       .list {
         height: 2.4rem;
         display: flex;
-        justify-content: center;
+        justify-content: space-around;
         align-items: center;
         .item {
-          margin: 0 .8rem;
-          width: 1rem;
           img {
-            width: 1rem;
-            height: 1rem;
+            width: .88rem;
+            height: .88rem;
+            margin-bottom: .29rem;
           }
           p {
             font-size: .24rem;
-            color: #464545;
+            color:rgba(0,0,0,1);
           }
         }
       }
