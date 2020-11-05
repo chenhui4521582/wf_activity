9b6c2cbb72786c129af4f6958d2f626d3ea3ad6d
朱允昌 <zhuyunchang@beeplay123.com>
2020-11-05 15:58:42 +0800

    stage

--- src/views/valuepackages/App.vue
+++ src/views/valuepackages/App.vue
@@ -3,10 +3,45 @@
     <p class="back" @click="back">返回</p>
     <p class="rule" @click="showRule">规则</p>
     <div class="container">
-      <div class="item" v-for="item in packages" @click="gotopay(item)"
-        :style="{ backgroundImage: `url(${getBackground(item)||''})`}">
+      <div class="item" v-for="(item,index) in packages" @click="gotopay(item)">
+        <p class="desc" v-html="'加赠'+item.content"></p>
+        <div class="middle">
+          <div class="icon-wrapper">
+            <img :src="item.productIcon|filter" alt="">
+          </div>
+          <div>
+            <p class="name">
+              {{item.amount|amountFilter}}
+            </p>
+            <p class="old-price">
+              原价：<span>￥{{item.originPrice}}</span>
+            </p>
+          </div>
+        </div>
+        <template
+          v-if="actInfo.needGiftSet&&actInfo.needGiftSet.length&&actInfo.needGiftSet.includes(index+1)">
+          <div class="btn blue">
+            下载APP,领取加赠
+          </div>
+        </template>
+        <template v-else>
+          <div class="btn orange">
+            ￥{{item.price}}
+          </div>
+        </template>
       </div>
     </div>
+    <div class="footer">
+      <template v-if="actInfo.type===1">
+        <p class="ddw-text">更多超值优惠，尽在多多玩APP</p>
+        <div class="download"></div>
+      </template>
+      <p>
+        多多玩APP由多多玩官方提供，<br />
+        如有问题咨询多多玩官方客服微信：<span>youxikefu2020</span>
+      </p>
+    </div>
+    <Modal v-model="isShowModal"></Modal>
     <rule v-if="isShowRule" :actInfo="actInfo" @close="isShowRule=false"></rule>
   </div>
 </template>
@@ -18,11 +53,21 @@ export default {
     return {
       actInfo: null,
       packages: [],
-      isShowRule: false
+      isShowRule: false,
+      isShowModal: false
     }
   },
   components: {
-    rule: () => import('./components/rule')
+    rule: () => import('./components/rule'),
+    Modal: () => import('./components/modal')
+  },
+  filters: {
+    amountFilter (value) {
+      if (value > 10000) {
+        return value / 10000 + '万金叶子'
+      }
+      return value + '金叶子'
+    }
   },
   methods: {
     back () {
@@ -39,9 +84,13 @@ export default {
     async getActInfo () {
       let { code, data } = await getActInfo()
       if (code == 200) {
+        data.needGiftSet = [1, 2]
         this.actInfo = data
         if (data.state == 1) {
           this.getPackages()
+        }
+        if (data.needGiftSet && data.needGiftSet.length) {
+          this.isShowModal = true
         }
       }
     },
@@ -61,11 +110,10 @@ export default {
     }
   },
   mounted () {
+    this.getActInfo()
     GLOBALS.marchSetsPoint('P_H5PT0295', {
       source_address: GLOBALS.getUrlParam('from') || ''
     })
-
-    this.getActInfo()
   }
 }
 </script>
@@ -122,14 +170,97 @@ export default {
     .item {
       display: inline-block;
       width: 3rem;
-      height: 2.31rem;
+      height: 2.4rem;
+      background: #fff8f7;
+      border: 1px solid #f16d59;
       border-radius: 0.26rem;
       box-sizing: border-box;
       margin-top: 0.2rem;
-      background-size: 100% 100%;
       &:nth-child(2n + 1) {
         margin-right: 0.2rem;
       }
+      .desc {
+        width: 2.72rem;
+        height: 0.34rem;
+        line-height: 0.32rem;
+        text-align: center;
+        font-size: 0.2rem;
+        color: #fff;
+        background: url(./images/desc-bg.png) no-repeat;
+        background-size: 100% 100%;
+        margin: auto;
+        font-weight: bold;
+        span {
+          color: #ffeb85;
+        }
+      }
+      .middle {
+        display: flex;
+        align-items: center;
+        margin-top: 0.14rem;
+        .icon-wrapper {
+          width: 1.18rem;
+          height: 1.08rem;
+          img {
+            width: 100%;
+            height: 100%;
+          }
+        }
+        .name {
+          color: #fa3727;
+          font-size: 0.26rem;
+          font-weight: bold;
+          line-height: 0.32rem;
+        }
+        .old-price {
+          color: #aaa;
+          font-size: 0.22rem;
+          line-height: 0.32rem;
+          span {
+            text-decoration: line-through;
+          }
+        }
+      }
+      .btn {
+        width: 2.58rem;
+        height: 0.7rem;
+        line-height: 0.7rem;
+        text-align: center;
+        margin: auto;
+        font-size: 0.24rem;
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+        &.blue {
+          background-image: url('./images/blue-btn-bg.png');
+          color: #0c6871;
+        }
+        &.orange {
+          background-image: url('./images/orange-btn-bg.png');
+          color: #ff4200;
+          font-weight: bold;
+        }
+      }
+    }
+  }
+  .footer {
+    text-align: center;
+    color: #8d0b01;
+    font-size: 0.2rem;
+    margin-top: 0.22rem;
+    span {
+      color: #fee95a;
+    }
+    .ddw-text {
+      color: #ffd31f;
+      font-size: 0.3rem;
+    }
+    .download {
+      width: 2.58rem;
+      height: 0.78rem;
+      background-image: url(./images/ddw-download.png);
+      background-repeat: no-repeat;
+      background-size: 100% 100%;
+      margin: 0.26rem auto 0.44rem;
     }
   }
 }
---
+++ src/views/valuepackages/components/modal.vue
@@ -0,0 +1,502 @@
+<template>
+  <div class="modal-warp" v-show="show" @touchmove.self.prevent>
+    <transition name="mask">
+      <div class="modal-mask" v-show="show"></div>
+    </transition>
+    <div class="modal-content">
+      <div class="header">
+        <span>下载多多玩APP 领取充值加赠</span>
+      </div>
+      <div class="content">
+        <div class="bind-header">
+          <div class="step" :class="{'active': step == 1}">
+            <div class="wrap">
+              <div class="icon">
+                <img src="../images/phone-icon.png" alt="">
+              </div>
+            </div>
+            <p v-if="step == 1">第1步<br>绑定手机号</p>
+            <p v-if="step == 2">第1步<br>{{hidePhone}}绑定成功</p>
+          </div>
+          <div class="step" :class="{'active': step == 2}">
+            <div class="wrap">
+              <div class="icon">
+                <img src="../images/down-icon.png" alt="">
+              </div>
+            </div>
+            <p>第2步<br>下载并登录多多玩</p>
+          </div>
+          <div class="line">
+            <div class="progress" :style="{width: countWidth}"></div>
+          </div>
+        </div>
+        <!-- 绑定手机号 -->
+        <div class="bind-phone" v-if="step == 1">
+          <div class="binding-warp">
+            <div class="input-wrap">
+              <input v-model.trim="phone" maxlength="11" type="tel" placeholder="手机号">
+            </div>
+            <div class="input-wrap">
+              <input v-model.trim="code" maxlength="6" type="tel" placeholder="验证码">
+              <div class="get-code" @click="getCode">
+                <div class="code-warp">{{countdownText}}</div>
+              </div>
+            </div>
+          </div>
+          <div class="submit" @click="saveBinding">立即绑定</div>
+          <div class="explain">绑定手机号，加赠才能发放到账</div>
+        </div>
+        <!-- 绑定完成 -->
+        <div class="bind-finish" v-if="step == 2">
+          <div class="explain">
+            用绑定手机号登录多多玩，即能领取加赠
+          </div>
+          <div v-if="isIOS" class="submit" v-clipboard:copy="copyText" v-clipboard:success="onCopy"
+            v-clipboard:error="onError">复制前往</div>
+          <div v-else class="submit" @click="toMiddle">立即下载</div>
+          <!-- <div class="copy">
+          复制下载链接
+        </div> -->
+          <div class="explain1">
+            游戏服务由多多玩官方提供，如有问题<br>
+            咨询多多玩客服微信：youxikefu2020
+          </div>
+        </div>
+      </div>
+      <div class="close-icon" @click="closeModal"></div>
+    </div>
+  </div>
+</template>
+<script>
+import utils from '@/common/js/utils.js'
+import { getPhoneBindInfo, bindMobilePhone, getPhoneCode } from '../utils/api'
+export default {
+  name: 'Modal',
+  data () {
+    return {
+      show: this.value,
+      step: 1,
+      countdown: 60,
+      countdownText: '获取验证码',
+      phone: null,
+      code: null,
+      copyText: 'https://wap.beeplaying.com/ddwgame/'
+    }
+  },
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    },
+    centerScroll: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    countWidth () {
+      if (this.step == 2) {
+        return `100%`
+      }
+      return ''
+    },
+    hidePhone () {
+      var reg = /^(\d{3})\d{4}(\d{4})$/
+      return this.phone.replace(reg, '$1****$2')
+    },
+    isIOS () {
+      return utils.isIOS()
+    }
+  },
+  watch: {
+    value (val) {
+      this.show = val
+    },
+    show (val) {
+      this.$emit('input', val)
+      if (val && !this.centerScroll) {
+        utils.ScrollNoMove()
+      } else {
+        utils.ScrollMove()
+      }
+    }
+  },
+  methods: {
+    closeModal () {
+      this.show = false
+      this.$emit('on-close', 0)
+    },
+    // 关闭弹框
+    close () {
+      this.show = false
+      this.$emit('on-close', 1)
+    },
+    // 确认按钮
+    save () {
+      this.$emit('on-save')
+    },
+    onCopy () {
+      this.$toast.show({ isOneLine: true, duration: 1500, message: '链接已复制，请在浏览器打开。' })
+    },
+    onError () {
+      this.$toast.show({ isOneLine: true, duration: 1500, message: '复制失败' })
+    },
+    // 前往中间页
+    toMiddle () {
+      let channel = localStorage.getItem('APP_CHANNEL')
+      this.$toast.show({ isOneLine: true, duration: 1500, message: '正在下载多多玩，请稍后' }, () => {
+        switch (channel) {
+          case '100096':
+            parent.location.href = 'https://wap.beeplaying.com/m/apk/kucaiba-100096-1.0.7.apk'
+            break
+          case '100097':
+            parent.location.href = 'https://wap.beeplaying.com/m/apk/hk_ddw_100097.apk'
+            break
+          case '100098':
+            parent.location.href = 'https://wap.beeplaying.com/m/apk/qm_ddw_100098.apk'
+            break
+          case '100039':
+            parent.location.href = 'https://wap.beeplaying.com/m/apk/hk_ddw_100097.apk'
+            break
+          case '100042':
+            parent.location.href = 'https://wap.beeplaying.com/m/apk/qm_ddw_100098.apk'
+            break
+          default:
+            parent.location.href = 'https://wap.beeplaying.com/m/apk/duoduowan_100030_1.0.2.apk'
+        }
+      })
+    },
+    /** 获取绑定手机数据 **/
+    getBindPhone () {
+      getPhoneBindInfo().then(res => {
+        const { code, data } = res
+        if (code === 200) {
+          /** 没有绑定手机号的时候打开弹框 **/
+          const { existsFlag, phone } = data
+          if (existsFlag) {
+            this.phone = phone
+            this.step = 2
+          } else {
+            this.step = 1
+          }
+        }
+      })
+    },
+    // 绑定手机号码
+    async saveBinding () {
+      let phoneReg = /^[1][3,4,5,6,7,8,9][0-9]{9}$/
+      if (this.phone === '') {
+        this.$toast.show({ isOneLine: true, duration: 1500, message: '请输入手机号码' })
+        return
+      }
+      if (!phoneReg.test(this.phone)) {
+        this.$toast.show({ isOneLine: true, duration: 1500, message: '请输入正确的手机号码' })
+        return
+      }
+      if (this.code === '') {
+        this.$toast.show({ isOneLine: true, duration: 1500, message: '请输入短信验证码' })
+        return
+      }
+      const { code, data, message } = await bindMobilePhone(this.phone, this.code)
+      if (code === 200) {
+        this.$toast.show({ isOneLine: true, duration: 1500, message: '绑定成功' }, () => {
+          this.step = 2
+        })
+      } else {
+        this.$toast.show({ isOneLine: true, duration: 1500, message: message })
+      }
+    },
+    // 获取短信验证吗
+    async getCode () {
+      let phoneReg = /^[1][3,4,5,6,7,8,9][0-9]{9}$/
+      if (this.countdown !== 60) {
+        return
+      }
+      if (this.phone === '') {
+        this.$toast.show({ isOneLine: true, duration: 1500, message: '请输入手机号码' })
+        return
+      }
+      if (!phoneReg.test(this.phone)) {
+        this.$toast.show({ isOneLine: true, duration: 1500, message: '请输入正确的手机号码' })
+        return
+      }
+      const { code, data, message } = await getPhoneCode(this.phone)
+      if (code === 200) {
+        this.$toast.show({ isOneLine: true, duration: 1500, message: '短信验证码已发送，注意查收' })
+        this.countdownFn()
+      } else {
+        this.$toast.show({ isOneLine: true, duration: 1500, message: message })
+        clearInterval(this.time)
+      }
+    },
+    // 验证码倒计时计算
+    countdownFn () {
+      if (this.countdown === 0) {
+        this.countdown_text = '重新获取'
+        this.countdown = 60
+        this.code = ''
+        clearTimeout(this.time)
+        return false
+      } else {
+        this.countdown--
+        if (this.countdown === 0) {
+          this.countdownText = '重新获取'
+        } else {
+          this.countdownText = this.countdown + 's'
+        }
+        this.time = setTimeout(() => {
+          this.countdownFn()
+        }, 1000)
+      }
+    }
+  },
+  created () {
+    this.getBindPhone()
+  }
+}
+</script>
+<style lang="less" scoped>
+* {
+  box-sizing: border-box;
+}
+.modal-warp {
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  z-index: 99;
+}
+.modal-mask {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+  z-index: 1;
+  overflow: hidden;
+  pointer-events: none;
+}
+.modal-content {
+  position: absolute;
+  width: 4.7rem;
+  min-height: 2rem;
+  z-index: 2;
+  background-color: #ffffff;
+  border-radius: 0.32rem;
+  top: 42%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  .header {
+    margin-top: 0.3rem;
+    width: 100%;
+    position: relative;
+    font-size: 0.28rem;
+    font-weight: bold;
+    color: #000000;
+    line-height: 0.4rem;
+    text-align: center;
+    &:not(.notitile) {
+      height: 0.4rem;
+      margin-top: 0.36rem;
+    }
+  }
+  .content {
+    padding: 0.08rem 0.3rem 0.23rem;
+    .bind-header {
+      position: relative;
+      margin: 0.1rem auto 0.3rem;
+      width: 4.1rem;
+      display: flex;
+      justify-content: space-between;
+      .step {
+        position: relative;
+        z-index: 3;
+        text-align: center;
+        font-size: 0.2rem;
+        color: #bbbbbb;
+        .wrap {
+          margin: 0 auto 0.15rem;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 0.9rem;
+          height: 0.9rem;
+          border-radius: 50%;
+          .icon {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 0.72rem;
+            height: 0.72rem;
+            border-radius: 50%;
+          }
+        }
+        p {
+          white-space: nowrap;
+        }
+        &:nth-child(1) {
+          .icon {
+            background: #ffbd82;
+            img {
+              width: 0.26rem;
+              height: 0.4rem;
+            }
+          }
+        }
+        &:nth-child(2) {
+          .icon {
+            background: #ffe7a9;
+            img {
+              width: 0.33rem;
+              height: 0.3rem;
+            }
+          }
+        }
+        &.active {
+          color: #000000;
+          .wrap {
+            background: #ffd7b3;
+          }
+          .icon {
+            background: #ff7800;
+          }
+        }
+      }
+      .line {
+        position: absolute;
+        top: 0.42rem;
+        left: 0;
+        width: 2.9rem;
+        height: 0.06rem;
+        background: #ffe7a9;
+        .progress {
+          width: 1.8rem;
+          height: 0.06rem;
+          background: #ff7800;
+        }
+      }
+    }
+    .bind-phone {
+      overflow: hidden;
+      .binding-warp {
+        padding-top: 0.3rem;
+        border-top: 0.01rem solid #eeeeee;
+        .input-wrap {
+          margin-bottom: 0.2rem;
+          display: flex;
+          justify-content: space-between;
+          background: #f7f7f7;
+          border-radius: 0.16rem;
+          input {
+            padding: 0.1rem 0 0.1rem 0.2rem;
+            width: 100%;
+            height: 0.7rem;
+            line-height: 0.5rem;
+            font-size: 0.24rem;
+            color: #000;
+            background: transparent;
+          }
+          input::-webkit-input-placeholder {
+            color: #cdcdcd;
+          }
+          input::-moz-input-placeholder {
+            color: #cdcdcd;
+          }
+          input::-ms-input-placeholder {
+            color: #cdcdcd;
+          }
+          &:last-child {
+            input {
+              width: 1.64rem;
+            }
+          }
+          .code-warp {
+            margin-top: 0.15rem;
+            width: 1.53rem;
+            height: 0.4rem;
+            font-size: 0.24rem;
+            color: #ff7800;
+            text-align: center;
+            line-height: 0.4rem;
+            border-left: 0.01rem solid #e6e6e6;
+          }
+        }
+      }
+      .submit {
+        margin: 0 auto 0.2rem;
+        width: 4.1rem;
+        height: 0.7rem;
+        line-height: 0.7rem;
+        background: #ff4141;
+        text-align: center;
+        font-size: 0.24rem;
+        color: #ffffff;
+        border-radius: 0.16rem;
+      }
+      .explain {
+        text-align: center;
+        font-size: 0.2rem;
+        color: #bbbbbb;
+      }
+    }
+    .bind-finish {
+      padding-top: 0.13rem;
+      border-top: 0.01rem solid #eeeeee;
+      .explain {
+        margin-bottom: 0.27rem;
+        font-size: 0.24rem;
+        color: #000000;
+      }
+      .submit {
+        margin: 0 auto 0.2rem;
+        width: 4.1rem;
+        height: 0.7rem;
+        line-height: 0.7rem;
+        background: #ff4141;
+        text-align: center;
+        font-size: 0.24rem;
+        color: #ffffff;
+        border-radius: 0.16rem;
+      }
+      .explain1 {
+        text-align: center;
+        font-size: 0.2rem;
+        color: #bbbbbb;
+      }
+    }
+  }
+  .close-icon {
+    position: absolute;
+    height: 0.18rem;
+    width: 0.18rem;
+    top: 0.2rem;
+    right: 0.2rem;
+    text-align: center;
+    line-height: 0.4rem;
+    background: url('../images/small-close.png') no-repeat;
+    background-size: 100% 100%;
+    i {
+      font-size: 0.18rem;
+      color: #888888;
+    }
+  }
+}
+// 动画
+.mask-enter {
+  opacity: 0;
+}
+.mask-enter-active {
+  transition: opacity 0.3s;
+}
+.mask-leave-to {
+  opacity: 0;
+}
+.mask-leave-active {
+  transition: opacity 0.3s;
+}
+</style>
+
+
